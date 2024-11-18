52900faea4b42e9535c76dbce9133e58e0c1da72
MickePros <Mikael_Pros@hotmail.com>
2024-11-18 23:09:12 +0100

    Add SeedData for User and Admin roles and users.

---
+++ Garage 3/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Garage_3.Data;
+
+namespace Garage_3.Extensions
+{
+    public static class ApplicationBuilderExtensions
+    {
+        public static async Task<IApplicationBuilder> SeedDataAsync(this IApplicationBuilder app)
+        {
+            using (var scope = app.ApplicationServices.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var context = services.GetRequiredService<ApplicationDbContext>();
+
+                try
+                {
+                    await SeedData.Init(context, services);
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+            return app;
+        }
+    }
+}
