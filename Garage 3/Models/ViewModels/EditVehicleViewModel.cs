a08d8f2a37263c863a54cc72ff2fc6deae3fb6ac
Ivona Josipovic <josipovic.ivona@gmail.com>
2024-11-22 13:53:49 +0100

    Renamed the model

---
+++ Garage 3/Models/ViewModels/EditVehicleViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Garage_3.Models.ViewModels
+{
+    public class EditVehicleViewModel
+    {
+        [Display(Name = "License plate")]
+
+        //Add remote attribute when Robert is done
+        public string RegNr { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+        [Range(0, 18)]
+        public int Wheels { get; set; }
+
+        [Display(Name = "Vehicle type")]
+        public string VehicleType { get; set; }
+    }
+}
